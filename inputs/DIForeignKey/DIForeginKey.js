9037275906376295cb81efea934ac9defe621b55
lukasberns <lukas_berns15@yahoo.co.jp>
2011-08-30 00:55:37 +0900

    Automatically refresh the DIForeignKey options if it's a self-referencing key (e.g. tree-like relationships)

--- inputs/DIForeignKey/DIForeginKey.js
+++ inputs/DIForeignKey/DIForeginKey.js
@@ -9,6 +9,25 @@ Lets you pick a foreign key from another table using a <select>
 
 (function($) {
 
-Class("DIForeignKey", DISelect);
+Class("DIForeignKey", DISelect, {
+	init: function($super, defs) {
+		$super(defs);
+		
+		if (defs.table == Dibasic.tableName) {
+			// we will need to refresh the data on each change
+			Dibasic.DPDBInterface.observe(function() {
+				$.get(Dibasic.url({ action: defs.name }), function(data) {
+					if (!data[0].match(/^[\[\{]/)) {
+						// not json = error
+						alert('Something went wrong while refreshing the data in DIForeignKey: '+data);
+						return;
+					}
+					
+					defs.options = JSON.parse(data);
+				});
+			});
+		}
+	}
+});
 
 })(jQuery);
--- inputs/DIForeignKey/DIForeginKey.php
+++ inputs/DIForeignKey/DIForeginKey.php
@@ -19,32 +19,16 @@ class DIForeignKey extends DISelect {
 		if (!$order) {
 			$order = 'id';
 		}
-		$order = self::parseOrderColumn($order);
+		$this->order = self::parseOrderColumn($order);
 		
-		$table = $this->getOption('table');
-		$column = $this->getOption('column');
-		if (!is_array($column)) {
-			$column = array($column);
+		$this->table = $this->getOption('table');
+		$columns = $this->getOption('column');
+		if (!is_array($columns)) {
+			$columns = array($columns);
 		}
+		$this->columns = $columns;
 		
-		$q = "SELECT * FROM `$table` ORDER BY $order";
-		$qr = mysql_query($q) or trigger_error(mysql_error(), E_USER_ERROR);
-		
-		$options = array();
-		while ($r = mysql_fetch_assoc($qr)) {
-			$v = array();
-			foreach ($column as $col) {
-				if (!isset($r[$col])) {
-					trigger_error('The column "'.$col.'" provided by the "column" option was not found in the table', E_USER_ERROR);
-				}
-				
-				$v[] = $r[$col];
-			}
-			$options[$r['id']] = implode(', ', $v);
-		}
-		
-		$this->options['options'] = $options;
-		
+		$this->options['options'] = $this->getSelectOptions();
 		parent::init();
 	}
 	
@@ -67,5 +51,36 @@ class DIForeignKey extends DISelect {
 			$q[] = "`$column` $direction";
 		}
 		return implode(', ', $q);
+	}
+	
+	public function getSelectOptions() {
+		$q = "SELECT * FROM `$this->table` ORDER BY $this->order";
+		$qr = mysql_query($q) or trigger_error(mysql_error(), E_USER_ERROR);
+		
+		$options = array();
+		
+		$rules = $this->getOption('rules');
+		if (strpos($rules, 'required') === false or (is_array($rules) and in_array('required', $rules))) {
+			$options[0] = '— none —';
+		}
+		
+		while ($r = mysql_fetch_assoc($qr)) {
+			$v = array();
+			foreach ($this->columns as $col) {
+				if (!isset($r[$col])) {
+					trigger_error('The column "'.$col.'" provided by the "column" option was not found in the table', E_USER_ERROR);
+				}
+				
+				$v[] = $r[$col];
+			}
+			$options[$r['id']] = implode(', ', $v);
+		}
+		
+		return $options;
+	}
+	
+	public function act() {
+		// refresh
+		echo json_encode($this->getSelectOptions());
 	}
 }

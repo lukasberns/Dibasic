70a9c6921c713f79ada728a7df43775863733c8d
lukasberns <lukas_berns15@yahoo.co.jp>
2011-10-10 17:06:33 +0900

    Fix bug where numeric keys in DISelect options would result in the options not being sorted in Chrome

--- inputs/DISelect/DISelect.php
+++ inputs/DISelect/DISelect.php
@@ -2,9 +2,35 @@
 
 class DISelect extends DI {
 	public function init() {
-		if (!is_array($this->getOption('options'))) {
+		$opts = $this->getOption('options');
+		if (!is_array($opts)) {
 			die('Please set an array for the option “options” for a DISelect.');
 		}
+		
+		// we need to prefix keys to support numeric keys
+		$this->options['options'] = array(); // empty
+		foreach ($opts as $k => $v) {
+			$this->options['options']['_'.$k] = $v;
+		}
+		
+		if (isset($this->options['default'])) {
+			$this->options['default'] = '_'.$this->options['default'];
+		}
+	}
+	
+	public function processData(&$data, $id = -1) {
+		super::processData($data, $id);
+		
+		// $data is all received data
+		$value = &$data[$this->columnName];
+		$value = substr($value, 1); // remove "_" prefix
+	}
+	
+	public function willSendData(&$data) {
+		// $data will be sent to the js frontend
+		// modify it here if, e.g. you need to obfuscate it (see DISecureText)
+		$value = &$data[$this->columnName];
+		$value = '_'.$value;
 	}
 }
 
